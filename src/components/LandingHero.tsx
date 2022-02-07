d6fdc1357521e3674637f7f9d4fdaf8ff48acf49
Joschka Cesar <cesar@posteo.de>
2022-02-07 17:48:48 +0100

    Add tailwind

---
+++ src/components/LandingHero.tsx
@@ -0,0 +1,17 @@
+export const LandingHero = () => {
+  return (
+    <div className="grid grid-cols-2 h-screen justify-center items-center">
+      <div>
+        <h1 className="text-6xl font-bold">find your mates.</h1>
+        <p>
+          Neu in der Stadt? Keine Lust mehr auf Zweck-WG? Lerne Leute kennen mit
+          denen du zusammen leben mächtest: Sei ganz du selbst und finde deine
+          WG!
+        </p>
+      </div>
+      <div>
+        <p>gif</p>
+      </div>
+    </div>
+  );
+};
