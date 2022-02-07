1a58b1256795824054b4eae1a25586170fa422a3
Joschka Cesar <cesar@posteo.de>
2022-02-07 18:21:33 +0100

    Add landing hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import { LandingHero } from '../components/LandingHero';
-import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Mates</title>
-        <meta name="description" content="Mates landing page" />
-        <link rel="icon" href="../../public/favicon.ico" />
-      </Head>
-      <LandingHero />
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Mates</title>
+      <meta name="description" content="Mates landing page" />
+      <link rel="icon" href="../../public/favicon.ico" />
+    </Head>
+    <LandingHero />
+  </div>
+);
 
 export default Home;
