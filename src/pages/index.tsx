153752394f3146b977d7a6e5d113fef68c9c6608
Joschka Cesar <cesar@posteo.de>
2022-02-08 15:16:03 +0100

    Add AppHighlight

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { AppHighlight } from '../components/AppHighlight';
 import { LandingHero } from '../components/LandingHero';
 
 const Home: NextPage = () => (
@@ -14,6 +15,7 @@ const Home: NextPage = () => (
       />
     </Head>
     <LandingHero />
+    <AppHighlight />
   </div>
 );
 
