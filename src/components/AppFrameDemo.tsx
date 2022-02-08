8b5824cef10a869712a18a2d5ddfb1184311730c
Joschka Cesar <cesar@posteo.de>
2022-02-08 16:46:54 +0100

    Add iphone screenshot

---
+++ src/components/AppFrameDemo.tsx
@@ -0,0 +1,11 @@
+export const AppFrameDemo = () => (
+  <div className="flex h-screen justify-center items-center ">
+    <div className="h-2/3">
+      <img
+        src="/iPhoneFrame.png"
+        alt="iPhone frame with a screenshot of the mates app"
+        className="w-full h-full"
+      />
+    </div>
+  </div>
+);
--- src/components/AppHighlight.tsx
+++ src/components/AppHighlight.tsx
@@ -1,11 +1,11 @@
+import { AppFrameDemo } from './AppFrameDemo';
+
 export const AppHighlight = () => (
   <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-    <div className="flex">
-      <div className="flex flex-col justify-center p-24 md:p-10 lg:p-32">
-        <img src="/iPhoneFrame.png" alt="Mates Logo" className="max-h-screen" />
-      </div>
+    <div className="bg-candy">
+      <AppFrameDemo />
     </div>
-    <div className="flex flex-col justify-center p-24 md:p-10 lg:p-32">
+    <div className="flex flex-col justify-center p-24 md:p-10 lg:p-32 bg-ghost">
       <h1 className="text-6xl font-bold my-6">Lorem ipsum.</h1>
       <div className="flex flex-col">
         <p className="my-6">
